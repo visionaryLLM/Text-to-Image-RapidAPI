1065f4676c0c199a2580b152c35422ce1a37b11d
Prakhar Doneria <gamesprotec@gmail.com>
2024-03-16 04:16:27 +0000

    Removed Free Image Host

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import os
-import requests
 from flask import Flask, jsonify, request
 from gradio_client import Client
 
 app = Flask(__name__)
 
 GRADIO_URL = os.environ.get('GRADIO_URL')
-FREEIMAGE_API_KEY = os.environ.get('FREEIMAGE_API_KEY')
-FREEIMAGE_HOST_API_URL = "http://freeimage.host/api/1/upload/"
 
 # Helper function to check if parameter is within range
 def check_range(param, min_val, max_val):
     return min_val <= param <= max_val
 
 @app.route('/generate', methods=['GET'])
-def generate_and_upload_image():
+def generate_image():
     try:
         prompt = request.args.get('prompt')
         negative = request.args.get('negative')
@@ -52,19 +49,7 @@ def generate_and_upload_image():
 
         image_url = GRADIO_URL + result['url']
 
-        # Upload image to freeimage.host
-        if image_url is not None:
-            response = requests.get(FREEIMAGE_HOST_API_URL, params={'key': FREEIMAGE_API_KEY, 'source': image_url, 'format': 'json'})
-
-            # Parse the response
-            response_json = response.json()
-            if response_json.get("status_code") == 200:
-                img_url = response_json["image"]["url"]
-                return jsonify({"imgURL": img_url}), 200
-            else:
-                return jsonify({"error": "Failed to upload image"}), 500
-        else:
-            return jsonify({"error": "Image URL is None"}), 500
+        return jsonify({"imgURL": image_url}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
